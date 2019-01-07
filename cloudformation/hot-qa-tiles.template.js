ee9cb11723100c7a02d1719a54b054e7d2914ab0
Aruna Sankaranarayanan <aruna.evam@gmail.com>
2019-01-07 20:47:12 +0530

    update cfn template

--- cloudformation/hot-qa-tiles.template.js
+++ cloudformation/hot-qa-tiles.template.js
@@ -21,7 +21,7 @@ const resources = {
         MixedInstancesPolicy: {
           LaunchTemplate: {
             LaunchTemplateSpecification: {
-              LaunchTemplateName: cf.stackName,
+              LaunchTemplateName: cf.ref('HOTQATilesEC2LaunchTemplate'),
               Version: 1
             },
             Overrides: [{
@@ -49,26 +49,67 @@ const resources = {
       MinSize: 0,
       Recurrence: '30 10 * * ? *'
     }
-  }
-}
-const qaTilesS3Permissions = [
-    {
-        Action: [ 's3:ListBucket'],
-        Effect: 'Allow',
-        Resource: ['arn:aws:s3:::hot-qa-tiles']
-    },
-    {
-        Action: [
-            's3:GetObject',
-            's3:GetObjectAcl',
-            's3:PutObject',
-            's3:PutObjectAcl',
-            's3:ListObjects',
-            's3:DeleteObject'
-        ],
-        Effect: 'Allow',
-        Resource: [
-            'arn:aws:s3:::hot-qa-tiles/*'
-        ]
+  },
+  HOTQATilesEC2LaunchTemplate: {
+    Type: 'AWS::EC2::LaunchTemplate',
+    Properties: {
+      LaunchTemplateName: cf.join('-', [cf.stackName, 'ec2', 'launch', 'template']),
+      LaunchTemplateData: {
+        UserData: 'TODO',
+        InstanceInitiatedShutdownBehavior: 'terminate',
+        IamInstanceProfile: cf.ref('HOTQATilesEC2InstanceProfile'),
+        KeyName: 'mbtiles',
+        ImageId: 'ami-dd4496a5'
+      }
     }
-];
+  },
+  HOTQATilesEC2Role: {
+    Type: 'AWS::IAM::Role',
+    Properties: {
+      AssumeRolePolicyDocument: {
+        Version: "2012-10-17",
+        Statement: [ {
+          Effect: "Allow",
+          Principal: {
+             Service: [ "ec2.amazonaws.com" ]
+          },
+          Action: [ "sts:AssumeRole" ]
+        } ]
+      },
+      Policies: [ {
+        PolicyName: "S3Policy",
+        PolicyDocument: {
+          Version: "2012-10-17",
+          Statement:[
+          {
+              Action: [ 's3:ListBucket'],
+              Effect: 'Allow',
+              Resource: ['arn:aws:s3:::hot-qa-tiles']
+          },
+          {
+              Action: [
+                  's3:GetObject',
+                  's3:GetObjectAcl',
+                  's3:PutObject',
+                  's3:PutObjectAcl',
+                  's3:ListObjects',
+                  's3:DeleteObject'
+              ],
+              Effect: 'Allow',
+              Resource: [
+                  'arn:aws:s3:::hot-qa-tiles/*'
+              ]
+          }
+      ],
+      RoleName: cf.join('-', [cf.stackName, 'ec2', 'role'])
+    }
+  },
+  HOTQATilesEC2InstanceProfile: {
+     Type: "AWS::IAM::InstanceProfile",
+     Properties: {
+        Roles: cf.ref('HOTQATilesEC2Role'),
+        InstanceProfileName: cf.join('-', [cf.stackName, 'ec2', 'instance', 'profile'])
+     }
+  },
+
+};
