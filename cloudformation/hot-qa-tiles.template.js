fb8da1075edf84ccb7fcd981b7a6d0e4bac8853d
Aruna Sankaranarayanan <aruna.evam@gmail.com>
2019-01-08 11:44:24 +0530

    cron

--- cloudformation/hot-qa-tiles.template.js
+++ cloudformation/hot-qa-tiles.template.js
@@ -41,7 +41,7 @@ const resources = {
       DesiredCapacity: 1,
       MaxSize: 1,
       MinSize: 0,
-      Recurrence: '30 10 * * ? *'
+      Recurrence: 'cron(30 10 * * ? *)'
     }
   },
   HOTQATilesEC2LaunchTemplate: {
