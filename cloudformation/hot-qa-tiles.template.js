2114e9ba9ecb20c2fb609db806bfc1d74c644910
Ramya Ragupathy <r.ragupathy85@gmail.com>
2019-01-10 14:54:24 +0530

    Node, npm, nvm, mbtiles-extracts setup

--- cloudformation/hot-qa-tiles.template.js
+++ cloudformation/hot-qa-tiles.template.js
@@ -60,6 +60,11 @@ const resources = {
           'sudo mount /dev/xvdb /tmp',
           'sudo yum install -y lvm2 wget vim tmux htop traceroute git gcc gcc-c++ make openssl-devel kernel-devel, mesa-libGL mesa-libGL-devel xorg-x11-server-Xorg.x86_64 libpcap pigz',
           'sudo yum --enablerepo epel install -y moreutils',
+          'sudo yum install nodejs npm --enablerepo=epel',
+          'sudo yum install nodejs --enablerepo=epel-testing',
+          'curl https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash',
+          'nvm install v9',
+          'npm install mbtiles-extracts -g',
           'git clone https://github.com/mapbox/mason.git ~/.mason',
           'sudo ln -s ~/.mason/mason /usr/local/bin/mason',
           '~/.mason/mason install libosmium 2.13.1',
@@ -75,6 +80,7 @@ const resources = {
           'sudo chmod 777 hot-qa-tiles/',
           'cd hot-qa-tiles/',
           'screen -dmS "tippecanoe" bash -c "aws s3 cp s3://hot-qa-tiles/mbtiles-updated.sh .; sudo chmod 777 mbtiles-updated.sh;./mbtiles-updated.sh"'
+          
         ]),
         InstanceInitiatedShutdownBehavior: 'terminate',
         IamInstanceProfile: {
