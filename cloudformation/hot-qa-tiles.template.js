22ebe1a4e08e47afaeff092e527cca866f80a64e
Dakota Benjamin <dakota.benjamin@hotosm.org>
2020-07-13 09:43:43 -0400

    Update scheduled action recurrence to weekly

--- cloudformation/hot-qa-tiles.template.js
+++ cloudformation/hot-qa-tiles.template.js
@@ -63,7 +63,7 @@ const resources = {
       DesiredCapacity: 1,
       MaxSize: 1,
       MinSize: 0,
-      Recurrence: '15 7 1 * *'
+      Recurrence: '15 7 * * 1'
     }
   },
   HOTQATilesEC2LaunchTemplate: {
